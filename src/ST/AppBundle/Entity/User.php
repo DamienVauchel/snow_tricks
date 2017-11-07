77bd301a7019003fc8ca627eea4d689a02f4b8b2
DamienVauchel <damien.vauchel@gmail.com>
2017-11-07 19:26:07 +0100

    Profile started

--- src/ST/AppBundle/Entity/User.php
+++ src/ST/AppBundle/Entity/User.php
@@ -24,6 +24,18 @@ class User extends BaseUser
     private $image;
 
     /**
+     * @var string
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lastname;
+
+    /**
      * Set image
      *
      * @param \ST\AppBundle\Entity\Image $image
@@ -45,5 +57,53 @@ class User extends BaseUser
     public function getImage()
     {
         return $this->image;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
     }
 }
--- src/ST/AppBundle/Form/ProfileType.php
+++ src/ST/AppBundle/Form/ProfileType.php
@@ -3,13 +3,17 @@
 namespace ST\AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('image',          ImageType::class);
+        $builder
+            ->add('image',          ImageType::class, array('required' => false))
+            ->add('firstname',      TextType::class)
+            ->add('lastName',       TextType::class);
     }
 
     public function getParent()
